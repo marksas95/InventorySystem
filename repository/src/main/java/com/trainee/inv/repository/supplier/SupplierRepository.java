7a0870e25e2d8e4caf3a525f3746f08245708c20
Jigs <jigsterjava23@yahoo.com>
2018-05-25 21:04:06 +0800

    fixed format

--- repository/src/main/java/com/trainee/inv/repository/supplier/SupplierRepository.java
+++ repository/src/main/java/com/trainee/inv/repository/supplier/SupplierRepository.java
@@ -2,7 +2,6 @@ package com.trainee.inv.repository.supplier;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
-	
-	
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
 }
