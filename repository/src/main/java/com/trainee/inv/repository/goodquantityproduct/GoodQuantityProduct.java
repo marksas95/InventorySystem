69aa4d268e7915df55a857279d91936a6c3be704
Jigs <jigsterjava23@yahoo.com>
2018-05-28 13:34:54 +0800

    added good quantity

---
+++ repository/src/main/java/com/trainee/inv/repository/goodquantityproduct/GoodQuantityProduct.java
@@ -0,0 +1,73 @@
+package com.trainee.inv.repository.goodquantityproduct;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.trainee.inv.repository.product.Product;
+
+@Entity
+public class GoodQuantityProduct {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private Product product;
+	private int quantity;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GoodQuantityProduct other = (GoodQuantityProduct) obj;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "GoodQuantityProduct [id=" + id + ", product=" + product + ", quantity=" + quantity + "]";
+	}
+
+}
---
+++ repository/src/main/java/com/trainee/inv/repository/goodquantityproduct/GoodQuantityRepository.java
@@ -0,0 +1,7 @@
+package com.trainee.inv.repository.goodquantityproduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoodQuantityRepository extends JpaRepository<GoodQuantityProduct, Integer> {
+	
+}
