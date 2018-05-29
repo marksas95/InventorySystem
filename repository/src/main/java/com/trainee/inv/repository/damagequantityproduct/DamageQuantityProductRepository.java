8554fcd29e37c4c5f8b168fa26872124f08b321a
Jigs <jigsterjava23@yahoo.com>
2018-05-29 10:24:58 +0800

    Added Damage Quantity Products

---
+++ repository/src/main/java/com/trainee/inv/repository/damagequantityproduct/DamageQuantityProductRepository.java
@@ -0,0 +1,7 @@
+package com.trainee.inv.repository.damagequantityproduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DamageQuantityProductRepository extends JpaRepository<DamageQuantityProduct, Integer> {
+
+}
