455363b0cbebb2d0fdc345514fb19117687b9437
Jigs <jigsterjava23@yahoo.com>
2018-05-29 11:07:06 +0800

    A little edit

--- service/src/test/java/com/trainee/inv/service/product/ProductServiceTest.java
+++ service/src/test/java/com/trainee/inv/service/product/ProductServiceTest.java
@@ -1,6 +1,5 @@
 package com.trainee.inv.service.product;
 
-
 import java.util.List;
 
 import org.junit.Ignore;
@@ -17,14 +16,12 @@ import com.trainee.inv.repository.supplier.Supplier;
 import com.trainee.inv.service.category.CategoryService;
 import com.trainee.inv.service.supplier.SupplierService;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductServiceTest {
 
 	@Autowired
 	private ProductService productService;
-
 	@Autowired
 	private CategoryService categoryService;
 	@Autowired
@@ -46,21 +43,20 @@ public class ProductServiceTest {
 		product.setRemarks("wala langss");
 		product.setSerialNumber("0000000000000ss");
 		product.setActive(true);
-		
+
 		product.setUnitOfMeasurement("PCSss");
 		product.setVatable(false);
-		
+
 		Product createdProduct = productService.create(product);
 		System.out.println(createdProduct);
 	}
-	
+
 	@Test
 	@Ignore
 	public void createProductThatAlreadyExistsTest() {
 		Category category = categoryService.findByName("blossb");
 		Supplier supplier = supplierService.findByName("jigs");
 		Product product = new Product();
-
 
 		product.setCategory(category);
 		product.setSupplier(supplier);
@@ -70,18 +66,18 @@ public class ProductServiceTest {
 		product.setRemarks("wala langs");
 		product.setSerialNumber("0000000000000s");
 		product.setActive(false);
-		
+
 		product.setUnitOfMeasurement("PCSs");
 		product.setVatable(false);
-		
+
 		Product createdProduct = productService.create(product);
 		System.out.println(createdProduct);
 	}
-	
+
 	@Test
-//	@Ignore
+	// @Ignore
 	public void updateProductTest() {
-		
+
 		Product product = new Product();
 		product.setId(134);
 		product.setName("www");
