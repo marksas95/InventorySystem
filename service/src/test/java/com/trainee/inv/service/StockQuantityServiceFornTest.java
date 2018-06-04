ced9ac45cd09aefa0a18ad1e7f42baad8a784501
Jigs <jigsterjava23@yahoo.com>
2018-06-04 11:44:33 +0800

    Changes for testing

--- service/src/test/java/com/trainee/inv/service/StockQuantityServiceFornTest.java
+++ service/src/test/java/com/trainee/inv/service/StockQuantityServiceFornTest.java
@@ -31,13 +31,13 @@ public class StockQuantityServiceFornTest {
 	}
 	
 	@Test
-	@Ignore
+//	@Ignore
 	public void transferStockTest() {
-		stockQuantityServiceForm.transferStocks(9, 10, 7, 11, 75);
+		stockQuantityServiceForm.transferStocks(20, 23, 1, 21, 23);
 	}
 	
 	@Test
-//	@Ignore
+	@Ignore
 	public void stockInDamageQuantityProduct() {
 		stockQuantityServiceForm.stockInDamageQuantityProduct(13, 14, 4, 60);
 	}
